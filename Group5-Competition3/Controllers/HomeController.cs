256cb9e99f8655c9c412cf05eb50e6e370245019
Amer Amer <aamer@usf.edu>
2026-04-22 11:30:40 -0400

    Edited HomeController.cs

--- Group5-Competition3/Controllers/HomeController.cs
+++ Group5-Competition3/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Group5_Competition3.Models;
+using System.Xml.Schema;
 
 namespace Group5_Competition3.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly HttpClient _httpClient;
+
+    public HomeController(IHttpClientFactory httpClientFactory)
     {
-        return View();
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        string apiUrl = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fccc77c150454041886544b58f106d86";
+
+        HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+        string json = await response.Content.ReadAsStringAsync();
+
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        // var newsResponse = JsonSerializer.Deserialize<NewsResponse>(json, options);
+        var articles = new List<Article>();
+        return View(articles);
     }
 
     public IActionResult Privacy()
